160dbe7f1598f18f56482e720b9eb25a1f01033c
Hunter L. Allen <hunterlallen@protonmail.com>
2019-10-18 07:52:27 -0400

    Fix compilation
    Signed-off-by: Hunter L. Allen <hunterlallen@protonmail.com>

--- tf2_ros/include/tf2_ros/static_transform_component.hpp
+++ tf2_ros/include/tf2_ros/static_transform_component.hpp
@@ -1,8 +1,6 @@
 #ifndef TF2_ROS__STATIC_TRANSFORM_COMPONENT_HPP_
 #define TF2_ROS__STATIC_TRANSFORM_COMPONENT_HPP_
 
-#include <string_view>
-
 #include "rclcpp/clock.hpp"
 #include "rclcpp/time_source.hpp"
 #include "rclcpp/rclcpp.hpp"
@@ -16,17 +14,25 @@ namespace tf2_ros
 {
 class static_transform_component : public rclcpp::Node
 {
+public:
   explicit static_transform_component(
     double tx, double ty, double tz,
     double ax, double ay, double az, double aw,
-    std::string_view frame_id, std::string_view child_id,
-    rclcpp::NodeOptions options, std::string_view name);
+    const char * frame_id, const char * child_id,
+    rclcpp::NodeOptions options, const char * name);
 
   explicit static_transform_component(
     double tx, double ty, double tz,
     double r, double p, double y,
-    std::string_view frame_id, std::string_view child_id,
-    rclcpp::NodeOptions options, std::string_view name);
+    const char * frame_id, const char * child_id,
+    rclcpp::NodeOptions options, const char * name);
+
+  explicit static_transform_component(
+    const rclcpp::NodeOptions & options, const char * name = "static_transform_component");
+
+  using TransformStamped = geometry_msgs::msg::TransformStamped;
+
+  void send_transform(const TransformStamped & transform);
 
   ~static_transform_component() override = default;
 
@@ -34,7 +40,7 @@ private:
   std::unique_ptr<tf2_ros::StaticTransformBroadcaster> m_broadcaster;
   rclcpp::TimeSource m_time_source;
   std::shared_ptr<rclcpp::Clock> m_clock;
-  geometry_msgs::msg::TransformStamped m_tf_msg;
+  TransformStamped m_tf_msg;
 };
 }
 #endif  // TF2_ROS__STATIC_TRANSFORM_COMPONENT_HPP_
--- tf2_ros/src/static_transform_component.cpp
+++ tf2_ros/src/static_transform_component.cpp
@@ -1,5 +1,6 @@
 #include "tf2_ros/static_transform_component.hpp"
-#include <string_view>
+#include "rclcpp/rclcpp.hpp"
+
 
 namespace tf2_ros
 {
@@ -7,14 +8,14 @@ namespace tf2_ros
 static_transform_component::static_transform_component(
   double tx, double ty, double tz,
   double ax, double ay, double az, double aw,
-  std::string_view frame_id, std::string_view child_id,
-  rclcpp::NodeOptions options, std::string_view name
+  const char * frame_id, const char * child_id,
+  rclcpp::NodeOptions options, const char * name
 ) : rclcpp::Node(name, options)
 {
   std::string tf2_name{std::string(name) + "_tf2"};
   m_broadcaster = std::make_unique<tf2_ros::StaticTransformBroadcaster>(std::make_shared<rclcpp::Node>(tf2_name, options));
   m_clock = std::make_shared<rclcpp::Clock>(RCL_ROS_TIME);
-  m_time_source.attach(m_clock);
+  m_time_source.attachClock(m_clock);
 
   m_tf_msg.transform.translation.x = tx;
   m_tf_msg.transform.translation.y = ty;
@@ -25,26 +26,26 @@ static_transform_component::static_transform_component(
   m_tf_msg.transform.rotation.z = az;
   m_tf_msg.transform.rotation.w = aw;
 
-  m_tf_msg.header.stamp = clock->now();
+  m_tf_msg.header.stamp = m_clock->now();
   m_tf_msg.header.frame_id = frame_id;
   m_tf_msg.child_frame_id = child_id;
 
   m_broadcaster->sendTransform(m_tf_msg);
 
-  RCLCPP_INFO(this->get_logger(), "Spinning until killed publishing %s to %s", frame_id, child_frame_id);
+  RCLCPP_INFO(this->get_logger(), "Spinning until killed publishing %s to %s", frame_id, child_id);
 }
 
 static_transform_component::static_transform_component(
   double tx, double ty, double tz,
   double r, double p, double y,
-  std::string_view frame_id, std::string_view child_id,
-  rclcpp::NodeOptions options, std::string_view name
+  const char * frame_id, const char * child_id,
+  rclcpp::NodeOptions options, const char * name
 ) : rclcpp::Node(name, options)
 {
   std::string tf2_name{std::string(name) + "_tf2"};
   m_broadcaster = std::make_unique<tf2_ros::StaticTransformBroadcaster>(std::make_shared<rclcpp::Node>(tf2_name, options));
   m_clock = std::make_shared<rclcpp::Clock>(RCL_ROS_TIME);
-  m_time_source.attach(m_clock);
+  m_time_source.attachClock(m_clock);
 
   m_tf_msg.transform.translation.x = tx;
   m_tf_msg.transform.translation.y = ty;
@@ -57,13 +58,24 @@ static_transform_component::static_transform_component(
   m_tf_msg.transform.rotation.z = quat.z();
   m_tf_msg.transform.rotation.w = quat.w();
 
-  m_tf_msg.header.stamp = clock->now();
+  m_tf_msg.header.stamp = m_clock->now();
   m_tf_msg.header.frame_id = frame_id;
   m_tf_msg.child_frame_id = child_id;
 
-  RCLCPP_INFO(this->get_logger(), "Spinning until killed publishing %s to %s", frame_id, child_frame_id);
+  RCLCPP_INFO(this->get_logger(), "Spinning until killed publishing %s to %s", frame_id, child_id);
 }
 
+static_transform_component::static_transform_component(const rclcpp::NodeOptions & options, const char * name)
+  : rclcpp::Node(name, options)
+{
+  std::string tf2_name{std::string(name) + "_tf2"};
+  m_broadcaster = std::make_unique<tf2_ros::StaticTransformBroadcaster>(std::make_shared<rclcpp::Node>(tf2_name, options));
+}
+
+void static_transform_component::send_transform(const TransformStamped & transform)
+{
+  m_broadcaster->sendTransform(transform);
+}
 }  // namespace tf2_ros
 
 #include "rclcpp_components/register_node_macro.hpp"
